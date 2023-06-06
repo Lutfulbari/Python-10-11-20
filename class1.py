35ac2b97349ddd1686ecdf18f968433736b8bd45
MOHAMMAD LUTFUL BARI <lutfulbarimusa@gmail.com>
2023-06-06 09:03:28 +0600

    first commit

---
+++ class1.py
@@ -0,0 +1,160 @@
+print('hello')
+
+name = "Lutful"
+
+name1 = "Lutful Bari Musa"
+
+name2 = 'Keywords are the reserved words in python'
+name2 = 'Keywords are the reserved words in python'
+
+keywords = "and,exec,not,assert,finally,or,break,for,pass,class,form,print,continue,global,raise,def,if,return,del,import,try,elif,in,while,else,is,with,except,lambda,yield,"
+
+
+num1 = 50
+num2 = 50.60
+num3 = 60
+num4 = 70.65
+
+b = True
+
+
+print(name)
+
+print(type(name))
+
+print(name1)
+print(name2)
+
+print(keywords)
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(b))
+
+con = int(num2)
+
+
+print(type(con))
+print(type(int(num4)))
+print(con)
+
+
+name = 'sidiq'
+name = 'harun'
+num1 = '69'
+b = True
+b = True
+c = int(b)
+b = True
+c = str(b)
+print(type(c))
+
+
+print(type(int(name)))
+print(type(int(b)))
+print(type(c))
+print(c)
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+num1 = 50
+num2 = 30
+
+result = num1 + num2
+
+print(result)
+
+num1 = 50
+num2 = 30
+
+result = num1 - num2
+
+print(result)
+
+
+num1 = int(input("Enter first number: " ))
+
+num2 = int(input('Enter second number: ' ))
+
+result = num1 // num2
+
+print(result)
+
+
+
+num1 = int(input("Enter first number: " ))
+
+num2 = int(input('Enter second number: ' ))
+
+result = num1 * num2
+
+print(result)
+
+
+
+num1 = int(input("Enter first number: " ))
+
+num2 = int(input('Enter second number: ' ))
+
+result = num1 ** num2
+
+print(result)
+
+
+num1 = int(input("Enter first number: " ))
+
+num2 = int(input('Enter second number: ' ))
+
+result = num1 / num2
+
+print(result)
+
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+num1 = int(input("Enter first number: " ))
+
+num2 = int(input('Enter second number: ' ))
+
+if num1 == num2:
+    print('Equal')
+
+
+
+e = True
+on = False
+off = False
+
+if e != True:
+    if on != True:
+        print('Light On')
+    else:
+        print('Light Off')
+else:
+    print('current Off')
+    
+e = True
+on = False
+# off = False
+
+if e != False:
+    if on != False:
+        print('Light On')
+    else:
+        print('Light Off')
+else:
+    print('current Off')
+
+english = 75
+
+if english > 79 and english < 101:
+    print('A+')
+
+elif english > 69:
+    print('A')
+
+
+company = 'Secure Ambit'
